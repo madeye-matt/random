006ad80032a46c502725edc3089bd3c6b6d7d592
matt <matt@madeye.com>
2014-05-09 17:38:58 +0100

    Tweaked project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject random "0.1.0-SNAPSHOT"
+(defproject com.madeye.clojure.random/core "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
